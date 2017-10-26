c2dacb6ca628be2b9db47f5b9eba3262a4331959
Ashish Singh Parihar <aparihar60@gmail.com>
2017-10-26 19:14:08 +0530

    NIT Arunachal Pradesh,Workshop 2017 CSE M.tech

---
+++ src/main/java/com/nitap/HelloController.java
@@ -0,0 +1,46 @@
+package com.nitap;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HelloController {
+
+	@RequestMapping("/option1")
+	public ModelAndView mymethod1(){
+		return new ModelAndView("option1","msg","Insert Data Here");
+	}
+	
+	@RequestMapping("/option2")
+	public ModelAndView mymethod2(){
+		return new ModelAndView("option2","msg","Hello First Spring");
+	}
+	
+	@RequestMapping("/option3")
+	public ModelAndView mymethod3(){
+		return new ModelAndView("option3","msg","Hello First Spring");
+	}
+	
+	@RequestMapping("/option4")
+	public ModelAndView mymethod4(){
+		return new ModelAndView("option4","msg","Hello First Spring");
+	}
+	
+	@RequestMapping("/option5")
+	public ModelAndView mymethod5(){
+		return new ModelAndView("option5","msg","Hello First Spring");
+	}
+	
+	@RequestMapping("/option6")
+	public ModelAndView mymethod6(){
+		return new ModelAndView("option6","msg","Hello First Spring");
+	}
+	
+	@RequestMapping("/option7")
+	public ModelAndView mymethod7(){
+		return new ModelAndView("option7","msg","Hello First Spring");
+	}
+	
+	
+}
---
+++ src/main/webapp/WEB-INF/jsp/option1.jsp
@@ -0,0 +1,23 @@
+
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<head><title>NIT Arunachal Pradesh, Govt. of India
+</title>
+
+
+
+
+</head>
+
+
+<body>
+<%@ include file="../../../index.jsp"%>
+Message is: ${msg}
+
+
+
+
+
+</body>
+</html>
